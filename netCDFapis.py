659a4c4dccb9352d0eaec4dfeb1bf8d1e0accf08
Gowdhaman Sadhasivam <gsadha2@uic.edu>
2015-02-05 15:22:13 -0600

    two function added

--- netCDFapis.py
+++ netCDFapis.py
@@ -2,10 +2,26 @@ from netCDF4 import Dataset
 
 
 def ndims():
- rootgrp = Dataset('tos_O1_2001-2002.nc','r')
- dims = rootgrp.dimensions
- dimlen = len(dims)
- print (str(dimlen))
+	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
+	dims = rootgrp.dimensions
+	dimlen = len(dims)
+ 	print("No. of Dimensions: " + str(dimlen))
+
+def get_global_dim_names():
+	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
+	dims = rootgrp.dimensions
+	print ("Global dimensions name: ")
+	for gnames in dims:
+		print gnames
+
+def get_global_dim_lens():
+	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
+	dims = rootgrp.dimensions
+	print ("Global dimensions length: ")
+	for gnames in dims:
+		print ( str(dims) + ":")
+
+		
 
 
-ndims()
+
