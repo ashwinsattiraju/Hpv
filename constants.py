e0b7513e4ec59b22bf859b46101c8e335c54ef41
Ashwin Sattiraju <ashwinsattiraju10@gmail.com>
2015-02-05 16:57:42 -0600

    Main completed

---
+++ constants.py
@@ -0,0 +1,43 @@
+# string constants
+# notes
+S_WELCOME_NOTE = 'Welcome!!'
+S_CHOICE_NOTE = 'Pick an option'
+S_QUIT_NOTE = 'type "q" to exit program'
+S_ENTER_NOTE = "Enter a number: "
+S_GOODBYE_NOTE = 'Bye!!'
+
+#errors
+S_INPUT_ERROR = 'test.py -i <inputfile>'
+S_CHOICE_ERROR = 'please enter a valid option'
+
+#options
+S_CHOICE_OPTION_1 = '1. '
+S_CHOICE_OPTION_2 = '2. '
+S_CHOICE_OPTION_3 = '3. '
+S_CHOICE_OPTION_4 = '4. '
+S_CHOICE_OPTION_5 = '5. '
+S_CHOICE_OPTION_6 = '6. '
+S_CHOICE_OPTION_7 = '7. '
+S_CHOICE_OPTION_8 = '8. '
+S_CHOICE_OPTION_9 = '9. '
+S_CHOICE_OPTION_10 = '10. '
+S_CHOICE_OPTION_11 = '11. '
+
+#vars
+S_HELP_VAR = '-h'
+S_INPUT_VAR = '-i'
+S_QUIT_VAR = 'q'
+S_ARGS_VAR = 'h:i:'
+
+# numeric constants
+N_OPTION_1 = '1'
+N_OPTION_2 = '2'
+N_OPTION_3 = '3'
+N_OPTION_4 = '4'
+N_OPTION_5 = '5'
+N_OPTION_6 = '6'
+N_OPTION_7 = '7'
+N_OPTION_8 = '8'
+N_OPTION_9 = '9'
+N_OPTION_10 = '10'
+N_OPTION_11 = '11'
--- mainProgram.py
+++ mainProgram.py
@@ -1,15 +1,96 @@
 # importing required modules
-import netCDFAPI
+import netCDFAPI as api
+import sys,getopt as options
+import constants as constants
+
+# inputfile name 
+inputFileName = ''
 
 # main function definition
 def main():
-    # define the variable 'current_time' as a tuple of time.localtime()
-    current_time = time.localtime() 
-    print(current_time)              # print the tuple
- 
-    # if the year is 2009 (first value in the current_time tuple)
-    if current_time[0] == 2009: 
-        print('The year is 2009')    # print the year
- 
-if __name__ == '__main__':     # if the function is the main function ...
-    main()
+	# print welcome note
+	print constants.S_WELCOME_NOTE
+	# read arguments and load file name
+	readArguments()
+	#initialize input choice to empty
+	input_choice = ''
+	while(True):
+		# display options list in API
+		displyOptions()
+		# read user input
+		input_choice = raw_input(constants.S_ENTER_NOTE)
+		# execute specific function
+		executeOption(input_choice)
+
+# function to disply options to user
+def displyOptions():
+	# display available options to user
+	print constants.S_CHOICE_NOTE
+	print constants.S_CHOICE_OPTION_1
+	print constants.S_CHOICE_OPTION_2
+	print constants.S_CHOICE_OPTION_3
+	print constants.S_CHOICE_OPTION_4
+	print constants.S_CHOICE_OPTION_5
+	print constants.S_CHOICE_OPTION_6
+	print constants.S_CHOICE_OPTION_7
+	print constants.S_CHOICE_OPTION_8
+	print constants.S_CHOICE_OPTION_9
+	print constants.S_CHOICE_OPTION_10
+	print constants.S_CHOICE_OPTION_11
+	print constants.S_QUIT_NOTE
+	
+# function to execute option
+def executeOption(choice):
+	# execute api function based on choice
+	if choice == constants.N_OPTION_1:
+		print 'option 1'
+	elif choice == constants.N_OPTION_2:
+		print 'option 2'
+	elif choice == constants.N_OPTION_3:
+		print 'option 2'
+	elif choice == constants.N_OPTION_4:
+		print 'option 2'
+	elif choice == constants.N_OPTION_5:
+		print 'option 2'
+	elif choice == constants.N_OPTION_6:
+		print 'option 2'
+	elif choice == constants.N_OPTION_7:
+		print 'option 2'
+	elif choice == constants.N_OPTION_8:
+		print 'option 2'
+	elif choice == constants.N_OPTION_9:
+		print 'option 2'
+	elif choice == constants.N_OPTION_10:
+		print 'option 2'
+	elif choice == constants.N_OPTION_11:
+		print 'option 2'
+	elif choice == constants.S_QUIT_VAR:
+		print constants.S_GOODBYE_NOTE
+		sys.exit()
+	else:
+		print constants.S_CHOICE_ERROR
+
+# function to read input from user 
+def readArguments():
+	# try to get name from args else throw error msg
+	try:
+		# get options and argument list values
+		opts, args = options.getopt(sys.argv[1:],constants.S_ARGS_VAR)
+	except options.GetoptError:
+		print constants.S_INPUT_ERROR
+		sys.exit(2)
+	# parse args and proceed accordingly  
+	for opt, arg in opts:
+		# disply help for h
+		if opt == constants.S_HELP_VAR :
+			print constants.S_INPUT_ERROR
+			sys.exit()
+		# store and dispay file name for i
+		elif opt == constants.S_INPUT_VAR:
+			inputFileName = arg
+	# print input file name     
+	print 'Input file is "', inputFileName,'"'
+
+# if the function is the main function start executing 
+if __name__ == '__main__':    
+	main()
