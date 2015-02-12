934ddeb985997581bd256e9fd0bd6cbe6bbb429c
Gowdhaman Sadhasivam <gsadha2@uic.edu>
2015-02-12 15:21:57 -0600

    Updated files

--- netCDFapis.py
+++ netCDFapis.py
@@ -1,4 +1,6 @@
 from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
 
 #return the number of dimensions 
 def ndims():
@@ -66,7 +68,52 @@ def get_num_of_points():
 	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
 	dims = rootgrp.dimensions
 	vars = rootgrp.variables
-	var = 'lon'
-	print vars[var][:]
+	var = 'lat'
+	numPoint = vars[var].shape
+	total = 1
+	for i in numPoint:
+		total *= (i+1)
+	print "Number of Grid points: " , total
+		
+# return the number of cells that have the variable 'var' defined
+def get_num_of_Cells():
+	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
+	dims = rootgrp.dimensions
+	vars = rootgrp.variables
+	var = 'tos'
+	numCells = vars[var].shape
+	total = 1
+	for i in numCells:
+		total *= i
+	print "Number of Cells : " , total
 
-get_num_of_points()
+#  return the data array for the variable 'var'
+def get_data():
+	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
+	dims = rootgrp.dimensions
+	vars = rootgrp.variables
+	var = 'tos'
+	data_array = np.array(vars[var][:])
+	print data_array
+
+# plot the histogram of the variable 'var'
+def plot_histogram(): 
+	rootgrp = Dataset('tos_O1_2001-2002.nc','r')
+	dims = rootgrp.dimensions
+	vars = rootgrp.variables
+	var = 'lon'
+
+	data_array = np.array(vars[var][:])
+
+	# For Tos variable give range as (270,310) - kelvin unit
+	if var == 'tos':
+		y , x= np.histogram(data_array,bins=50, range = (270,310))
+	else:
+		 y , x= np.histogram(data_array,bins=50, range = None)
+
+	width = 0.9 * (x[1] - x[0])
+	center = (x[:-1] + x[1:]) /2
+
+	plt.bar(center, y, align='center', width=width)
+	plt.show()
+
