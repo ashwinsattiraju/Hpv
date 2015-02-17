b0e1c2e1bf64ed4a79fdc2cc2899e8318f6807cd
Gowdhaman Sadhasivam <gsadha2@uic.edu>
2015-02-17 17:03:01 -0600

    test

--- 3examples/1.py
+++
@@ -1,9 +0,0 @@
-#import numpy module
-import numpy
-import matplotlib.pyplot as plt
-# crearw an array from 1 to 100
-A = numpy.array(range(1,101))
-#create a box plot
-plt.boxplot(A)
-#disply boxplot
-plt.show()
